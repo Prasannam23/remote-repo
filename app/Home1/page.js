dcbe2c877bf6a1e9c21a47c92f4285379db633b3
Prasannam23 <123781385+Prasannam23@users.noreply.github.com>
2024-02-20 23:55:34 +0530

    Update page.js

--- app/Home1/page.js
+++ app/Home1/page.js
@@ -5,7 +5,7 @@ import Constant from '../Constant/page';
 const page = () => {
   return (
     <>
-     <Navbar/>
+    
      <body className="w-[1523.50px] h-[917.30px] relative bg-white">
         <div className='back w-[1523.50px] h-[917.30px] opacity-50 absolute'>
         <Constant/>
